1c1e7e17e5b983d8834a705795a90c33bc2d440c
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2020-10-29 23:12:48 +0300

    infrastructural things+

--- src/Reflection/Reflector.ts
+++ src/Reflection/Reflector.ts
@@ -32,29 +32,8 @@ export class Reflector {
             if (field.isElement) {
                 const elementField = field as IElementField;
                 const element = document.createElement(elementField.tag || "div");
-                const elementReflection = new ElementReflection();
-                elementReflection.element = element;
-                elementReflection.manifest = elementField;
-
-                element.className = elementField.class;
-                if (elementField.attributes?.length > 0) {
-                    for (let attributeIndex = 0; attributeIndex < elementField.attributes.length; attributeIndex++) {
-                        const attribute = elementField.attributes[attributeIndex];
-                        element.setAttribute(attribute.key, attribute.value)
-                    }
-                }
-                console.info(elementField.eventBindings)
-                for (const key in elementField.eventBindings || {}) {
-                    if (Object.prototype.hasOwnProperty.call(elementField.eventBindings, key)) {
-                        const methods = elementField.eventBindings[key];
-                        for (let methodIndex = 0; methodIndex < methods?.length; methodIndex++) {
-                            const method = methods?.[methodIndex];
-                            element.addEventListener(key, (event => { method(elementReflection, event) }));
-                        }
-                    }
-                }
-                baseElement.appendChild(element);
-                this.expand(element, elementField.content);
+                const elementReflection = new ElementReflection(elementField, element, this, baseElement);
+               
             }
             if (field.isSection) {
                 this.expand(baseElement, field.content);
