0535b3fce8af63b582e7038b54a5310a872ee428
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2020-11-08 01:55:26 +0300

    documented and a field added to sections

--- src/Definitions/ManifestoField/SectionField/ISectionFieldInitial.ts
+++ src/Definitions/ManifestoField/SectionField/ISectionFieldInitial.ts
@@ -1,10 +1,30 @@
 import { IInitialFied } from "../InitialField/IInitialField";
 import { IInitialFiedBase } from "../InitialField/IInitialFieldBase";
 
-
+/**
+ * Section constructor parameters for Class entity of Section.
+ */
 export interface ISectionFieldBase extends IInitialFiedBase {
     content: IInitialFied[];
+    /**
+     * When get value from Reflector in any mode, that section's value is will be there with that name
+     */
     name?: string;
+    /**
+     * If section is root, must be true.
+     */
     root?: boolean;
+    /**
+     * When get value from Reflector in submit mode, not contained in values if Ghost is true. Othervise, will be there.
+     */
+    ghost?: boolean;
+    /**
+     * When get value from Reflector in raw mode, returns section's value as array, or object if that arraySectionRaw is not true
+     */
     arraySectionRaw?: boolean;
+
+    /**
+     * 
+     */
+    providerFromHam? : (hamObject : Object) => any;
 }
--- src/Definitions/ManifestoField/SectionField/SectionField.ts
+++ src/Definitions/ManifestoField/SectionField/SectionField.ts
@@ -4,6 +4,10 @@ import { ISectionFieldBase } from "./ISectionFieldInitial";
 import { ISectionField } from "./ISectionField";
 import { ObjectFieldTransfer } from "../../Utils/ObjectFieldTransfer";
 
+/**
+ * Sections are represents object, array. In same time if there is provider, can be returned any specified value.
+ * Sections is not fillable directly by user. This is a difference than @class InputField
+ */
 export class SectionField extends InitialFied implements ISectionField {
     readonly isSection = true;
     sectionName: string;
